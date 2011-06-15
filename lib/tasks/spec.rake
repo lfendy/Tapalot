6ee6354b147acaadd170d39c0472b6934b78fa6c
Liauw Fendy <lfendy@gmail.com>
2011-06-15 12:30:01 +1000

    added failing test for stopping

---
+++ lib/tasks/spec.rake
@@ -0,0 +1,6 @@
+
+namespace :spec do
+  task :default do
+    p "YEAH"
+  end
+end
--- spec/tapalot_spec.rb
+++ spec/tapalot_spec.rb
@@ -1,40 +1,119 @@
 require 'spec_helper.rb'
 
+
+=begin
+* how to keep going
+* how to stop -- need to introduce timer
+=end
+
+
 class Tapalot
+
+  attr_reader :current_tap, :current_measure
 
   def initialize opts
     @opts = opts
   end
 
-  def tap 
+  def reset
+    @current_tap = 1
+    @current_measure = 1
+  end
+
+  def tap measures=1, &block
     beats = @opts[:beats]
-    callback = @opts[:callback]
-    beats.times {|x| callback.call((x+1).to_s)}
+    bpm = @opts[:bpm]
+    interval = 60.0 / bpm.to_f
+    measures.times do
+      beats.times do |x|
+        sleep(interval)
+        yield (x+1)
+      end
+    end
   end
 end
 
 
 describe Tapalot do
 
+  describe "#reset" do
+    it "should reset current tap and measure count to 1" do
+      t = Tapalot.new({})
+      t.reset
+      t.current_tap.should == 1
+      t.current_measure.should == 1
+    end
+  end
+
   describe "#tap" do
-    it "should tap the correct beats for each measure" do
-      to_tap = 'mockproc'
-      beats = 3
-      mock(to_tap).call('1')
-      mock(to_tap).call('2')
-      mock(to_tap).call('3')
-      opts =  {
-                :beats => beats,
+
+    before :each do
+      @opts =  {
+                :beats => 3,
                 :measure => 4,
-                :bpm => 100,
-                :callback => to_tap
-              }
-      t = Tapalot.new opts
-      t.tap 
+                :bpm => 180
+               }
+    end
+  
+    it "should tap the correct beats for each measure" do
+      mockproc = 'mockproc'
+      mock(mockproc).call(1)
+      mock(mockproc).call(2)
+      mock(mockproc).call(3)
+
+      @opts[:bpm]      = 1800
+      t = Tapalot.new @opts
+
+      t.tap {|x| mockproc.call(x)}
     end
 
+    it "should tap beat at appropriate times" do
+      bpm = 1800
+      bps = bpm.to_f / 60.0
+      interval = 1.0 / bps
+      now = Time.now
+      check_for_timing = lambda { |x| 
+                ((Time.now - now) - (interval*x)).abs.should < 0.001
+               }
+      @opts[:bpm]      = bpm
+      t = Tapalot.new @opts
+      t.tap &check_for_timing
+    end
 
+    it "should tap for correct number of measures" do
+      mockproc = 'mockproc'
+      mock(mockproc).call(1).times(2)
+      mock(mockproc).call(2).times(2)
+      mock(mockproc).call(3).times(2)
 
+      @opts[:bpm]      = 1800
+      t = Tapalot.new @opts
+
+      t.tap(2) {|x| mockproc.call(x)}
+        
+    end
+
+  end
+
+  describe "#stop" do
+    it "should stop tapping when told to" do
+      mockproc = 'mockproc'
+      mock(mockproc).call(1).times(2)
+      mock(mockproc).call(2).times(2)
+      mock(mockproc).call(3).times(2)
+
+      @opts =  {
+                :beats => 3,
+                :measure => 4,
+                :bpm => 1800
+               }
+      t = Tapalot.new @opts
+
+      t.tap(10) {|x| mockproc.call(x)}
+      sleep(0.2)
+      t.stop
+      
+    end
   end
 
 
