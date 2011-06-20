2e285aca9deab79d6e768de8f0da12347a833c45
Liauw Fendy <lfendy@gmail.com>
2011-06-20 10:20:02 +1000

    slowly building up conductor

--- spec/conductor_spec.rb
+++ spec/conductor_spec.rb
@@ -3,9 +3,22 @@ require 'metronome'
 
 class Conductor
 
-  attr_reader :metronomes
-  def initialize song
-    @metronomes = song.sections.map {|x| Metronome.new x.rhythm }
+  attr_reader :metronomes, :current_session, :song
+  def initialize song=nil
+    @metronomes = song.sections.map {|x| Metronome.new x.rhythm } unless song.nil?
+    @song = song
+  end
+
+  def reset
+    @current_session = 0
+  end
+
+  def update_count
+    @current_session += 1
+  end
+
+  def run
+
   end
 
 end
@@ -34,10 +47,26 @@ describe Conductor do
       c.metronomes.count.should == 2
       c.metronomes.include?('m1').should be_true
       c.metronomes.include?('m2').should be_true
+      c.song.should == song
     end
-
-    
 
   end
 
+  describe "#reset" do
+    it "should reset current session to 0" do
+      c = Conductor.new
+      c.reset
+      c.current_session.should == 0
+    end
+  end
+
+  describe "#update_count" do
+    it "should increase current session index by 1" do
+      c = Conductor.new
+      c.reset
+      c.update_count
+      c.current_session.should == 1
+    end
+  end
 end
+
